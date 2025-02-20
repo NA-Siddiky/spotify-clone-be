fdaf1f22b602d958b6db84b58ec468383954affa
NA-Siddiky <siddiky.academic@gmail.com>
2025-02-20 14:21:54 +0600

    imp: refactor code

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'checking default repo';
+    return 'hello nestjs';
   }
 }
